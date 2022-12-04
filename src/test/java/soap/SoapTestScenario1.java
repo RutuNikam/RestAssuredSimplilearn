8531d6fbdf0d3ebfd2b572c1e1572b53138c65c3
Rutuja Nikam <rutujasnikam@gmail.com>
2022-12-04 11:13:43 +0530

    Fifth Commit

--- src/test/java/soap/SoapTestScenario1.java
+++ src/test/java/soap/SoapTestScenario1.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import groovyjarjarantlr4.v4.runtime.tree.xpath.XPath;
+
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import reusable.Reusable;
@@ -12,6 +14,9 @@ import io.restassured.http.ContentType;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.path.xml.config.XmlPathConfig;
 import static io.restassured.path.xml.config.XmlPathConfig.xmlPathConfig;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.xml.HasXPath.hasXPath;
 
 import java.rmi.server.RemoteObject;
 
@@ -21,7 +26,7 @@ import reusable.Reusable;
 public class SoapTestScenario1 {
 	
 	public static Reusable reObj;
-	
+	/*
 	@BeforeClass
 	public void TestDataSetup()
 	{
@@ -73,4 +78,95 @@ public class SoapTestScenario1 {
 	      System.out.println("The CelsiusToFahrenheitResult is==>"+CelsiusToFahrenheitResult);
 	      Assert.assertEquals(Fahrenheit, CelsiusToFahrenheitResult);
 	}
+	
+	@Test
+	public void DoGetSoapCall()
+	{
+		//Positive Test Case
+		get("https://chercher.tech/sample/api/books.xml")
+		.then()
+		.assertThat()
+		.body("bookstore.book[0].title",equalTo("The Nightingale"))
+		.body("bookstore.book[0].price.hardcover",equalTo("570"))
+		.body("bookstore.book[1].price",equalTo("29.99"))
+		//.body("bookstore.book.price.title",containsString("Harry Potter"));
+		.body("bookstore.book[0].@category",equalTo("cooking"))
+		.body("bookstore.book[0].title.@lang",equalTo("en"))
+		.body("bookstore.book[1].@category",equalTo("children"));
+
+
+		//Negative Test Case
+		//get("https://chercher.tech/sample/api/books.xml").then().assertThat().body("bookstore.book[0].title",equalTo("The Nightingale1"));
+	
+	
+	}
+	
+	//Above method can be written in this way also
+	@Test
+	public void DoGetSoapCall1()
+	{
+		//Positive Test Case
+		get("https://chercher.tech/sample/api/books.xml")
+        .then().assertThat()
+        .body("bookstore.book[0].title",equalTo( "The Nightingale")
+        ,"bookstore.book[0].price.hardcover",equalTo("570")  
+        ,"bookstore.book[1].price",equalTo("29.99")
+        //.body("bookstore.book.title",containsString("Harry Potter"))
+        ,"bookstore.book[0].@category",equalTo("cooking")
+        ,"bookstore.book[0].title.@lang",equalTo("en") 
+        ,"bookstore.book[1].@category",equalTo("children"));	
+	
+	}
+	
+	@Test
+	//Using Xpath
+	public void DoGetSoapCall2()
+	{
+		  get("https://chercher.tech/sample/api/books.xml")
+          .then().assertThat()
+          .body(hasXPath("/bookstore/book[1]/title",containsString("The Nightingale")));
+
+	
+	}*/
+	
+	@Test
+	public void ValidateWithContains_SOAP()
+	{
+		Response response=get("https://chercher.tech/sample/api/books.xml");
+		String res=response.getBody().asString();
+		
+		XmlPath xml_path_obj=new XmlPath(res).using(xmlPathConfig().namespaceAware(false));
+		
+		//first get the count of node you want to test
+		int xmlpathcount=xml_path_obj.get("bookstore.book.title.size()");
+		System.out.println("Count is--> " +xmlpathcount);
+		
+		for (int a=0;a<xmlpathcount;a++)
+		{
+			String title=xml_path_obj.getString("bookstore.book["+a+"].title");
+			if(title.equals("Happry Potter"))
+			{
+				System.out.println("Yes,The Expected Text is available "+" Harry Potter");
+				break;
+			}
+			else
+			{
+				System.out.println("No,The Expected Text is available"+"Harry Potter");
+				break;
+			}
+						
+		}
+		
+		//Below if code is not good way of writing code
+		/*if(res.contains("Harry Potter"))
+		{
+			System.out.println("Yes,The Expected Text is available"+"Harry Potter");
+		}
+		else
+		{
+			System.out.println("No,The Expected Text is available"+"Harry Potter");
+		}*/
+	}
+	
+	
 }
